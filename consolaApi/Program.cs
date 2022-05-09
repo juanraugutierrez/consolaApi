e017031e0273c13f7787857070c770d0d70bad01
Juan Gutiérrez Calquín <juan.gutierrezcalquin@gmail.com>
2022-05-09 14:17:33 -0400

    39

--- consolaApi/Program.cs
+++ consolaApi/Program.cs
@@ -3,10 +3,80 @@ using System.Text;
 using System.Net.Http.Headers;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
+using System.Text.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 var userName = "servilift";
 var passwd = "etXk83Wtg56MLMJcwtuUgAQe";
-var url = "https://api.fieldbeat.com/fleets/servilift/tasks?from=1643673600000&until=1646092799000";
+
+
+
+
+string fechai =String.Empty;
+string fechaf = String.Empty;
+
+int fechaiu;
+int fechafu;
+
+
+
+Console.Write("Fecha inicial:");
+fechai = Console.ReadLine();
+#pragma warning disable CS8600 // Se va a convertir un literal nulo o un posible valor nulo en un tipo que no acepta valores NULL
+Console.Write("Fecha final:");
+fechaf = Console.ReadLine();
+#pragma warning restore CS8600 // Se va a convertir un literal nulo o un posible valor nulo en un tipo que no acepta valores NULL
+
+DateTime fechaii = Convert.ToDateTime(fechai);
+DateTime fechaff= Convert.ToDateTime(fechaf);
+
+fechaii = fechaii.ToUniversalTime();
+fechaff = fechaff.ToUniversalTime();
+
+
+
+TimeSpan epochTicks = new(new DateTime(1970, 1, 1).Ticks);
+
+
+//Creating Second Time Interval and Substracting its Value from the First One
+TimeSpan unixTicks = new TimeSpan(fechaii.Ticks) - epochTicks;
+
+//Converting time interval to seconds to represent Unix Timestamp
+Int64 unixTimestamp = (Int32)unixTicks.TotalSeconds;
+unixTimestamp *= 1000;
+
+
+TimeSpan unixTicksf = new TimeSpan(fechaff.Ticks) - epochTicks;
+
+//Converting time interval to seconds to represent Unix Timestamp
+Int64 unixTimestampf = (Int32)unixTicksf.TotalSeconds;
+unixTimestampf *= 1000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var url = "https://api.fieldbeat.com/fleets/servilift/tasks?from=" + unixTimestamp +"&until="+unixTimestampf;
 
 using var client = new HttpClient();
 
@@ -23,18 +93,22 @@ var obj = Newtonsoft.Json.JsonConvert.DeserializeObject(content);
 var f = Newtonsoft.Json.JsonConvert.SerializeObject(obj, Newtonsoft.Json.Formatting.Indented);
 Console.WriteLine(f);
 
-string path = "C:\\paso\\file-ahora.json";
+
+
+
+
+string path = "C:\\paso\\file-ahora" + Convert.ToString(fechaii).Replace(":", "") + " " + Convert.ToString(fechaff).Replace(":", "") + ".json";
 //string Text = "Hello, Hi, ByeBye";
 File.WriteAllText(path, f);
-StringBuilder sbguias = new StringBuilder();
+StringBuilder sbguias = new();
 
 
-JsonTextReader reader = new JsonTextReader(new StringReader(f));
+JsonTextReader reader = new(new StringReader(f));
 while (reader.Read())
 {
     if (reader.Value != null)
     {
-        String linea = string.Concat("Token: ", reader.TokenType, " Valor: ", reader.Value);
+        String linea = string.Concat("Token: ", reader.TokenType, "\t Valor: ", reader.Value);
         Console.WriteLine("Token: {0}, Valor: {1}", reader.TokenType, reader.Value);
 #pragma warning disable CS8602 // Desreferencia de una referencia posiblemente NULL.
         var c1 = reader.Value.ToString().Trim();
@@ -60,6 +134,56 @@ while (reader.Read())
         sbguias.Append('\n');
     }
 }
-path = "C:\\paso\\file-texto.txt";
+path = "C:\\paso\\file-texto"+ Convert.ToString(fechaii).Replace(":","")+" "+ Convert.ToString(fechaff).Replace(":","")+".txt";
+
+var path2 = path;
 //string Text = "Hello, Hi, ByeBye";
 File.WriteAllText(path, sbguias.ToString());
+
+
+
+
+//JArray reader = new(new StringReader(f));
+
+//JArray jsonPreservar = JArray.Parse(f);
+StreamReader reader1 = new StreamReader(f);
+JArray jsonPreservar = JArray.Parse(reader1.ReadToEnd());
+
+foreach (JObject jsonOperaciones in jsonPreservar.Children<JObject>())
+{
+    //Aqui para poder identificar las propiedades y sus valores
+    /*foreach (JProperty jsonOPropiedades in jsonOperaciones.Properties())
+    {
+        string propiedad = jsonOPropiedades.Name;
+        if (propiedad.Equals("idgoOperacion"))
+        {
+            var idgoOperacion = Convert.ToInt32(jsonOPropiedades.Value);
+        }
+    }*/
+    //Aqui puedes acceder al objeto y obtener sus valores
+    JObject data = JObject.Parse(jsonPreservar[0].ToString());
+    var rut = Convert.ToInt32(data["rut"]);
+    Console.Write($"rut: ");
+    Console.ReadKey();
+}
+
+Console.WriteLine("valor original " + fechaii);
+Console.WriteLine("Valor de la fecha inicial es:" + unixTimestamp);
+
+
+
+
+Console.WriteLine("Valor de la fecha final es:" + fechaff);
+Console.WriteLine("Valor calculado final:" + unixTimestampf);
+
+Console.ReadKey();
+
+
+Console.Write("Valor a convertir en fecha: ");
+Int64 valor=Console.Read();
+
+
+Console.WriteLine(valor);
+Console.WriteLine(DateTimeOffset.FromUnixTimeMilliseconds(valor));
+
+Console.Read();
